30193e30972e27e74233d83187ab6bea6113ef53
Martijn Pieters <info@zopatista.com>
2008-12-15 15:45:54 +0000

    We need to instanciate the test for every thread to pass along the right thread/cycle/concurrency info to be logged. Otherwise we end up with everything being logged to cycle 1, concurrency 0 and useless graphs.

--- src/collective/funkload/loop.py
+++ src/collective/funkload/loop.py
@@ -10,7 +10,8 @@ class LoopTestRunner(BenchRunner.LoopTestRunner):
             test._TestCase__testMethodName, cycle, cvus, thread_id)
         threading.Thread.__init__(
             self, target=self.run, name=meta_method_name, args=())
-        self.test = test
+        # Instanciate test anew with meta_method_name to ensure correct logging
+        self.test = test.__class__(meta_method_name, test.options)
         self.color = not options.no_color
         self.sleep_time = sleep_time
         self.debug = debug
