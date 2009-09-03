629c2ebe0cb65ff5b3d8de20dedb840e9bc12682
Ross Patterson <me@rpatterson.net>
2009-09-03 00:40:44 +0000

    Another bug with reports re-generated unnecessarily.  Really need coverage on this.

--- src/collective/funkload/label.py
+++ src/collective/funkload/label.py
@@ -89,14 +89,13 @@ def process_axis(options, found, labels, labels_vs, label):
             test_vs_d = tests_vs[test]
             path_vs = test_vs_d['report']
             diffs_vs = test_vs_d['diffs']
-            if os.path.dirname(path_vs) not in paths_vs:
+            if path_vs not in paths_vs:
                 diff_path = report.build_diff_report(
                     options, abs_path,
                     os.path.join(options.output_dir, path_vs))
                 diff_path = os.path.basename(diff_path)
-
-            diffs_vs[label] = diff_path
-            diffs[label_vs] = diff_path
+                diffs_vs[label] = diff_path
+                diffs[label_vs] = diff_path
 
 def run(options):
     found = report.results_by_label(options.output_dir)
