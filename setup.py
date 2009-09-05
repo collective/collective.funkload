29958f13c37452f38681691a193c60ebbbc17b89
Ross Patterson <me@rpatterson.net>
2009-09-05 03:20:29 +0000

    Integrate docs into long_descriptions

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ version = '0.2'
 setup(name='collective.funkload',
       version=version,
       description="Zope and Plone focussed extensions to funkload",
-      long_description=open(os.path.join("src", "collective", "funkload",
-        "README.txt")).read() + "\n" +
-        open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open(os.path.join(
+          "src", "collective", "funkload", "README.txt")).read() +
+          "\n" + open(os.path.join(
+          "src", "collective", "funkload", "labels.txt")).read() +
+          "\n" + open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
