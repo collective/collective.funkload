32c48ef25b0493bc1439ec168e28f4c9d19d5827
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2014-06-20 12:46:29 +0200

    add dexterity method for adding contents

--- src/collective/funkload/testcase.py
+++ src/collective/funkload/testcase.py
@@ -10,19 +10,20 @@ from funkload import utils
 
 from collective.funkload import bench
 
+
 class FLTestCase(FunkLoadTestCase.FunkLoadTestCase):
 
     def _funkload_init(self):
         """Pull the conf file based on the test case class"""
         self.in_bench_mode = bench.in_bench_mode
-        
+
         config_directory = os.path.dirname(
             sys.modules[self.__class__.__module__].__file__)
         config_path = os.path.join(config_directory,
                                    self.__class__.__name__ + '.conf')
         config_path = os.path.abspath(config_path)
         if not os.path.exists(config_path):
-            config_path = "Missing: "+ config_path
+            config_path = "Missing: " + config_path
         config = ConfigParser.ConfigParser()
         config.read(config_path)
         self._config = config
@@ -49,9 +50,10 @@ class FLTestCase(FunkLoadTestCase.FunkLoadTestCase):
 
         # init loggers
         self.logger = utils.get_default_logger(self.log_to, self.log_path)
-        self.logger_result = utils.get_default_logger(log_to="xml",
-                                                log_path=self.result_path,
-                                                name="FunkLoadResult")
+        self.logger_result = utils.get_default_logger(
+            log_to="xml",
+            log_path=self.result_path,
+            name="FunkLoadResult")
 
         # init webunit browser (passing a fake methodName)
         self._browser = FunkLoadTestCase.WebTestCase(methodName='log')
@@ -62,7 +64,7 @@ class FLTestCase(FunkLoadTestCase.FunkLoadTestCase):
         time = datetime.datetime.now()
         stamp = time.isoformat()[:19].replace(
             ':', '').replace('-', '')
-        
+
         utils.close_logger("FunkLoad")
         log_path = os.path.splitext(self.log_path)
         self.logger = utils.get_default_logger(
@@ -83,33 +85,45 @@ class FLTestCase(FunkLoadTestCase.FunkLoadTestCase):
                 key, saxutils.quoteattr(str(value))))
         self._logr('\n'.join(xml), force=True)
 
+
 class PloneFLTestCase(FLTestCase):
 
     def plone_login(self, server_url, username, password, description):
         """Plone login action """
 
-        return self.post(server_url + "/login_form", params=[
-                        ['form.submitted', '1'],
-                        ['js_enabled', '0'],
-                        ['cookies_enabled', '0'],
-                        ['login_name', ''],
-                        ['pwd_empty', '0'],
-                        ['came_from', 'login_success'],
-                        ['__ac_name', username],
-                        ['__ac_password', password]],
-                        description=description)
+        return self.post(
+            server_url + "/login_form",
+            params=[
+                ['form.submitted', '1'],
+                ['js_enabled', '0'],
+                ['cookies_enabled', '0'],
+                ['login_name', ''],
+                ['pwd_empty', '0'],
+                ['came_from', 'login_success'],
+                ['__ac_name', username],
+                ['__ac_password', password]
+            ],
+            description=description)
 
-
-    def addContent(self, base_url, portal_type, params, description): 
+    def addContent(self, base_url, portal_type, params, description):
 
         portal_factory = self._browse(base_url + "/createObject?type_name=" + portal_type,
-                                      method='get', 
+                                      method='get',
                                       follow_redirect=False,
-                                      description = 'Get ' + portal_type + ' portal factory')
+                                      description='Get ' + portal_type + ' portal factory')
         edit_url = portal_factory.headers.get('Location')
         object_id = edit_url.split('/')[-2]
         params = dict(params)
         params['id'] = object_id
+        params = params.items()
+        object_created = self.post(edit_url, params=params, description=description)
+        new_object_id = object_created.url.split('/')[-2]
+        return new_object_id
+
+    def addContentDx(self, base_url, portal_type, params, description):
+
+        edit_url = base_url + "/++add++" + portal_type
+        params = dict(params)
         params = params.items()
         object_created = self.post(edit_url, params=params, description=description)
         new_object_id = object_created.url.split('/')[-2]
