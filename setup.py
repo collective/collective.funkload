10b4e447255d8b4a3497f25048ad81fef7e3086f
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2014-06-20 13:05:18 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -7,22 +7,20 @@ setup(name='collective.funkload',
       version=version,
       description="Zope and Plone focussed extensions to funkload",
       long_description=open(os.path.join(
-          "README.rst")).read() +
-          "\n" + open(os.path.join(
-          "src", "collective", "funkload", "labels.txt")).read() +
-          "\n" + open(os.path.join("docs", "HISTORY.txt")).read(),
+          "README.rst")).read() + "\n" + open(os.path.join(
+          "src", "collective", "funkload", "labels.txt")).read() + "\n" + open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='Ross Patterson',
       author_email='me@rpatterson.net',
-      url='http://pypi.python.org/pypi/collective.funkload',
+      url='https://github.com/collective/collective.funkload',
       license='GPL',
       packages=find_packages('src', exclude=['ez_setup']),
-      package_dir = {'':'src'},
+      package_dir={'': 'src'},
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
@@ -41,6 +39,6 @@ setup(name='collective.funkload',
               'fl-list = collective.funkload.report:main'],
           'zc.buildout': [
               'default = collective.funkload.recipe:TestRunner'],
-          },
+      },
       test_suite='collective.funkload',
       )
